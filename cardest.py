0eba0ddafa434d98e08cf56535645f5f2ec72a5c
FlysJoint <flysjoint@gmail.com>
2020-10-16 10:31:21 +0100

    added child classes and colour variable

--- cardest.py
+++ cardest.py
@@ -2,14 +2,27 @@
 
 class Car(object):
     avg_speed = 60.0
-    def __init__(self, make):
-      self.make = make
+    def __init__(self, model, colour):
+        self.model = model
+        self.colour = colour
 
     def eta(self, distance):
-        return distance / self.avg_speed
+        return 'Arrive in %sy style in %s hours' % (self.colour, str(distance / self.avg_speed))
 
-my_car  = Car('Aygo')
+class Toyota(Car):
+    avg_speed = 40.0
+
+class Ford(Car):
+    avg_speed = 50.0
+
+# todo
+# user choice of car
+# user entry of distance
+
+my_car  = Toyota('Toyota', 'Pink')
+your_car  = Ford('Ford', 'Purple')
 
 print(my_car.eta(100))
+print(your_car.eta(100))
 
 
